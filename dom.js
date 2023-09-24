3a1646ac67aaad0c315efcffeba61b87b69c5679
N-ishant <nishantupreti30@gmail.com>
2023-09-24 13:35:53 +0530

    Changes

--- dom.js
+++ dom.js
@@ -1,12 +1,32 @@
 // EXAMINE THE DOCUMENT OBJECT
+/*
+console.dir(document);
 console.log(document.title);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.all[10].textContent = "Hello");
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
+*/
 
-//Get Element By Id
+//Get Element By Id :::::::
+//console.log(document.getElementById('header-title'));
 let hTitle = document.getElementById('header-title');
 let mTitle = document.getElementById('main-header');
+//console.log(hTitle);
+//hTitle.textContent = 'Hello';
+//hTitle.innerText = 'Goodbye';
+//console.log(hTitle.innerText);
+//hTitle.innerHTML = '<h3>Hello</h3>';
 mTitle.style.borderBottom = 'solid 3px #000';
 
-//Get Element By Class
-let items = document.getElementsByClassName('list-group-item');
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'green';
+//Get Element By CLASSNAME ::::::
+//console.log(document.getElementsByClassName('title'));
+let addItemTitle = document.getElementsByClassName('title');
+addItemTitle[0].style.fontWeight = 'bold'; //Making Text Bold
+addItemTitle[0].style.color = 'green'; // Change Font Color to Green
